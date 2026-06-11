e344fef9e461aa260bee1933bd2bab23475799f2
Blaise Nduwimana <nduwimanablaise@gmail.com>
2026-06-11 14:24:20 +0200

    feat(profile): populate user info from ObjectBox, hide unavailable sections

--- app/src/main/java/rw/martinhardware/mymartin/ui/profile/ProfileFragment.java
+++ app/src/main/java/rw/martinhardware/mymartin/ui/profile/ProfileFragment.java
@@ -5,13 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import io.objectbox.Box;
+import rw.martinhardware.mymartin.MyApp;
 import rw.martinhardware.mymartin.auth.AuthActivity;
 import rw.martinhardware.mymartin.databinding.FragmentProfileBinding;
+import rw.martinhardware.mymartin.entities.User;
+import rw.martinhardware.mymartin.entities.User_;
 import rw.martinhardware.mymartin.viewmodel.AuthViewModel;
 
 public class ProfileFragment extends Fragment {
@@ -41,6 +46,29 @@ public class ProfileFragment extends Fragment {
                 requireActivity().finish();
             }
         });
+
+        loadUserProfile();
+    }
+
+    private void loadUserProfile() {
+        try {
+            Box<User> box = ((MyApp) requireActivity().getApplication()).getBoxStore().boxFor(User.class);
+            User u = box.query().equal(User_.isActive, true).build().findFirst();
+            if (u != null) {
+                binding.tvDriverName.setText(u.getName() != null ? u.getName() : "Driver");
+                String id = u.getUuid() != null ? "ID: " + u.getUuid() : (u.getEmail() != null ? u.getEmail() : "");
+                binding.tvDriverId.setText(id);
+                String phone = u.getPhoneNumber() != null ? u.getPhoneNumber() : u.getWhatsappNumber();
+                binding.tvPhoneNumber.setText(phone != null ? phone : "N/A");
+
+                binding.llVehicleRow.setVisibility(View.GONE);
+                binding.llDeliveryStats.setVisibility(View.GONE);
+                binding.llEditProfile.setVisibility(View.GONE);
+                binding.llSettings.setVisibility(View.GONE);
+            }
+        } catch (Exception e) {
+            Toast.makeText(getContext(), "Failed to load profile", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void logout() {
