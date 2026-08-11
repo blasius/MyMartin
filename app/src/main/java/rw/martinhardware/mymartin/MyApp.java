d2d30edfe3cbd7497c34703f439082c477db509e
Blaise Nduwimana <nduwimanablaise@gmail.com>
2026-08-11 08:07:23 +0200

    fix: disable dark mode, force light theme

--- app/src/main/java/rw/martinhardware/mymartin/MyApp.java
+++ app/src/main/java/rw/martinhardware/mymartin/MyApp.java
@@ -2,6 +2,8 @@ package rw.martinhardware.mymartin;
 
 import android.app.Application;
 
+import androidx.appcompat.app.AppCompatDelegate;
+
 import io.objectbox.BoxStore;
 import rw.martinhardware.mymartin.entities.MyObjectBox;
 
@@ -13,6 +15,7 @@ public class MyApp extends Application {
     public void onCreate() {
         super.onCreate();
         boxStore = MyObjectBox.builder().androidContext(this).build();
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); // Disable dark mode
     }
 
     public BoxStore getBoxStore() {
